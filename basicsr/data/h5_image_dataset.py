479165dc989945d6b5c5bd48216c355307ec90fb
JuWanMaeng <aodwndhks@gmail.com>
2025-03-26 07:02:44 +0000

    L1 loss

--- basicsr/data/h5_image_dataset.py
+++ basicsr/data/h5_image_dataset.py
@@ -97,7 +97,7 @@ class H5ImageDataset(data.Dataset):
         """
         if self.h5_file is None:
             self.h5_file = h5py.File(self.data_path, 'r')
-        return self.h5_file['B2V']['voxel{:09d}'.format(index)][:]
+        return self.h5_file['original_voxels']['voxel{:09d}'.format(index)][:]
 
 
     def __init__(self, opt, data_path, return_voxel=True, return_frame=True, return_gt_frame=True,
@@ -252,7 +252,7 @@ class H5ImageDataset(data.Dataset):
         # normalize voxel to [-1,1]
         # max_val = torch.max(torch.abs(voxel))
         # voxel = voxel / max_val
-        # voxel = voxel/ 1800
+        voxel = voxel/ 1800
 
         if self.vox_transform:
             random.seed(seed)
